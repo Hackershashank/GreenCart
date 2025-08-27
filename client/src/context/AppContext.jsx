02ebc30bc63f1113f9226d67868e2a64abf15f65
Shashank <shashanksingh14370@gmail.com>
2025-08-27 18:18:13 +0530

    upto add-address page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,6 +14,7 @@ export const AppContextProvider=({children})=>{
     const [showUserLogin, setShowUserLogin]=useState(false);
     const [products,setProducts]=useState([]);
     const [cartItems,setCartItems]=useState({});
+    const [searchQuery,setSearchQuery]=useState({});
 
     // fetch all products
     const fetchProducts=async()=>{
@@ -53,11 +54,32 @@ export const AppContextProvider=({children})=>{
         setCartItems(cartData);
     }
 
+    // Get cart items count
+    const getCartCount=()=>{
+        let totalCount=0;
+        for(const item in cartItems){
+            totalCount+=cartItems[item];
+        }
+        return totalCount;
+    }
+
+    // Get cart total amount
+    const getCartAmount=()=>{
+        let totalAmount=0;
+        for(const items in cartItems){
+            let itemInfo=products.find((product)=>product._id===items);
+            if(cartItems[items]>0){
+                totalAmount+=itemInfo.offerPrice * cartItems[items];
+            }
+        }
+        return Math.floor(totalAmount*100)/100;
+    }
+
     useEffect(()=>{
         fetchProducts();
     },[]);
 
-    const value={navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems};
+    const value={navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems,searchQuery,setSearchQuery,getCartAmount,getCartCount};
 
     return <AppContext.Provider value={value}>
         {children}
