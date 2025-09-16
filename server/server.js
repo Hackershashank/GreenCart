3453860d7f18b1b0f21d8c90371d26607898f457
Shashank <shashanksingh14370@gmail.com>
2025-09-17 01:04:38 +0530

    added address related api

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
+import addressRouter from './routes/addressRoute.js';
 
 const app=express();
 
@@ -33,6 +34,7 @@ app.use("/api/user",userRouter);
 app.use("/api/seller",sellerRouter);
 app.use("/api/product",productRouter);
 app.use("/api/cart",cartRouter);
+app.use("/api/address",addressRouter);
 
 app.listen(PORT,(req,res)=>{
     console.log(`Server is running of port ${PORT}`);
