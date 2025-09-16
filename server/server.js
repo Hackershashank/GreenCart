6168aac8499f82373c9359dc1dda2115fdb3ba40
Shashank <shashanksingh14370@gmail.com>
2025-09-17 00:46:36 +0530

    upto updateCart api

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 import cookieParser from 'cookie-parser';
 import express from 'express'
 import cors from 'cors';
-import connectDB from './configs/db.js';
+
 import 'dotenv/config';
+
+import connectDB from './configs/db.js';
+import connectCloudinary from './configs/cloudinary.js';
+
+import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 const app=express();
 
 const PORT=process.env.PORT||4000;
 await connectDB();
+await connectCloudinary();
 
 // allow multiple origins
 const allowedOrigins=['http://localhost:5173']
@@ -20,6 +29,10 @@ app.use(cors({origin: allowedOrigins, credentials:true}));
 app.get("/",(req,res)=>{
     res.send("API is working")
 })
+app.use("/api/user",userRouter);
+app.use("/api/seller",sellerRouter);
+app.use("/api/product",productRouter);
+app.use("/api/cart",cartRouter);
 
 app.listen(PORT,(req,res)=>{
     console.log(`Server is running of port ${PORT}`);
